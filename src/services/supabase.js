a1219d209bfd5f51cccb402e2408a54d3bee0d02
Abolfazl Sohrabi <abolfazlsohr8bi@gmail.com>
2025-08-04 17:49:26 -0700

    Uploading Images to Supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://yxvrhzypeqsslbwbddji.supabase.co";
+export const supabaseUrl = "https://yxvrhzypeqsslbwbddji.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4dnJoenlwZXFzc2xid2JkZGppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1OTU2ODksImV4cCI6MjA2OTE3MTY4OX0.Up-ms-mRiDM-1x5HGmljiLBILJdNrUGvzY2kq6OOBjU";
 
