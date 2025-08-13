1c9ff199f2d67158e4e640c8bf1bd8b0d6d49e50
Abolfazl Sohrabi <abolfazlsohr8bi@gmail.com>
2025-08-13 12:37:54 +0330

    Converting the Modal to a Compoud component

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,3 +1,4 @@
+import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
@@ -51,18 +52,37 @@ const Button = styled.button`
   }
 `;
 
-function Modal({ children, onClose }) {
+const ModalContaxt = createContext();
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState("");
+  const close = () => setOpenName("");
+  const open = setOpenName;
+  return (
+    <ModalContaxt.Provider value={{ openName, close, open }}>
+      {children}
+    </ModalContaxt.Provider>
+  );
+}
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContaxt);
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContaxt);
+  if (name !== openName) return null;
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={onClose}>
+        <Button onClick={close}>
           <HiXMark />
         </Button>
-        <div>{children}</div>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     document.body
   );
 }
-
+Modal.Open = Open;
+Modal.Window = Window;
 export default Modal;
