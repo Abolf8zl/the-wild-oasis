841492c370945f9d6ed896787d2658da4d6d2cb2
Abolfazl Sohrabi <abolfazlsohr8bi@gmail.com>
2025-08-20 21:33:48 +0330

    API-Side Pagination Bookings

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,6 +1,7 @@
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
+import { PAGE_SIZE } from "../utils/constants";
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -58,7 +59,6 @@ const PaginationButton = styled.button`
   }
 `;
 
-const PAGE_SIZE = 10;
 function Pagination({ count }) {
   const [searchParams, setSearchParams] = useSearchParams();
   const currentPage = !searchParams.get("page")
@@ -91,7 +91,7 @@ function Pagination({ count }) {
         </PaginationButton>
         <PaginationButton
           onClick={nextPage}
-          disabled={currentPage === currentPage}
+          // disabled={currentPage === currentPage}
         >
           <span>Next</span>
           <HiChevronRight />
