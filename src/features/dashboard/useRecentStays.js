600302f99ec96eed26483ecfec579c82ce72fc57
Abolfazl Sohrabi <abolfazlsohr8bi@gmail.com>
2025-08-23 22:08:55 +0330

    Displaying Statistics

--- src/features/dashboard/useRecentStays.js
+++ src/features/dashboard/useRecentStays.js
@@ -7,16 +7,12 @@ export function useRecentStays() {
   const [searchParams] = useSearchParams();
   const numDays = !searchParams.get("last") ? 7 : searchParams.get("last");
   const queryDate = subDays(new Date(), numDays).toISOString();
-  const {
-    isLoading,
-    data: stays,
-    error,
-  } = useQuery({
+  const { isLoading, data: stays } = useQuery({
     queryFn: () => getStaysAfterDate(queryDate),
     queryKey: ["stays", `last-${numDays}`],
   });
   const confirmedStays = stays?.filter(
     (stay) => stay.status === "checked-in" || stay.status === "checked-out"
   );
-  return { isLoading, stays, confirmedStays };
+  return { isLoading, stays, confirmedStays, numDays };
 }
