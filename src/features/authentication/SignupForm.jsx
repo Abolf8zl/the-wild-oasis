dbfc1db6153a6ae40a9699045e465526a888ce7a
Abolfazl Sohrabi <abolfazlsohr8bi@gmail.com>
2025-08-23 13:34:09 +0330

    User Sign Up

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,20 +3,30 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSignup } from "./useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { signup, isLoading } = useSignup();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
-  function onSubmit() {}
+  function onSubmit({ fullName, email, password }) {
+    signup(
+      { fullName, email, password },
+      {
+        onSettled: () => reset(),
+      }
+    );
+  }
   return (
-    <Form onSubmit={handleSubmit()}>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow label="Full name" error={errors?.fullName?.message}>
         <Input
           type="text"
           id="fullName"
           {...register("fullName", { required: "This field is required" })}
+          disabled={isLoading}
         />
       </FormRow>
 
@@ -31,6 +41,7 @@ function SignupForm() {
               message: "Pleas Provide a valid email address",
             },
           })}
+          disabled={isLoading}
         />
       </FormRow>
 
@@ -48,6 +59,8 @@ function SignupForm() {
               message: "Password needs a  minimum of 8 characters",
             },
           })}
+          disabled={isLoading}
+          s
         />
       </FormRow>
 
@@ -60,15 +73,16 @@ function SignupForm() {
             validate: (value) =>
               value === getValues().password || "password needs to match",
           })}
+          disabled={isLoading}
         />
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button disabled={isLoading} variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>Create new user</Button>
       </FormRow>
     </Form>
   );
